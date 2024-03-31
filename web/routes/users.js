cdc8d6f20dfbb78d40ca448fec69263a134b2568
tanishmohanta <87773686+tanishtt@users.noreply.github.com>
2024-03-31 19:43:01 +0530

    Update users.js

--- web/routes/users.js
+++ web/routes/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 /* GET users listing. */
-router.get('/users/tanish', function (req, res, next) {
+router.get('/users/tanishmohanta99', function (req, res, next) {
   res.send('respond with a resource')
 })
 
